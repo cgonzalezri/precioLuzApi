57cc80f7ed167efbb0bf0f47ec5dcf2c98e1f72d
Carlos González <cgonzalezr@outlook.com>
2021-08-18 19:49:34 +0200

    Allow CORS

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,16 @@ namespace precioLuzApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+            });
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
@@ -61,6 +71,8 @@ namespace precioLuzApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthentication();
 
